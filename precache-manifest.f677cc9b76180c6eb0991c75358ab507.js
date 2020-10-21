85bdf3c15f3c6078eade52ed890f74c075f7d4e4
SubinPark <subinpark@adminusers-MBP.attlocal.net>
2020-10-21 11:28:12 -0400

    Updates

---
+++ precache-manifest.f677cc9b76180c6eb0991c75358ab507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "288c1976dcf1c438a189d6670b74bd23",
+    "url": "/gbpages_practice/index.html"
+  },
+  {
+    "revision": "842fa61a5054f05df2dd",
+    "url": "/gbpages_practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5dba6a99472053313ff",
+    "url": "/gbpages_practice/static/js/2.a751668b.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/gbpages_practice/static/js/2.a751668b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "842fa61a5054f05df2dd",
+    "url": "/gbpages_practice/static/js/main.25538ee4.chunk.js"
+  },
+  {
+    "revision": "31e19811920bb1a8fba2",
+    "url": "/gbpages_practice/static/js/runtime-main.34892078.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gbpages_practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
